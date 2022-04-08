e4546d696fb6d94dcc1a8325872cc2fcdeba42f8
Ksenia Pilshchikova <ksushapi@gmail.com>
2022-04-08 14:26:22 +0200

    task 1 and 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+player.on("loaded", onPageReload);
+   
+function onTimeUpdate(){
+        player.getCurrentTime().then(time => localStorage.setItem("videoplayer-current-time", time));
+}
+
+function onPageReload() {
+        const currentTime = localStorage.getItem("videoplayer-current-time");
+        player.setCurrentTime(currentTime);
+}
